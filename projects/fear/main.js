1a22a35c49f0377dfa88737dc64574366b9725bb
pana005 <pana005@newschool.edu>
2019-04-05 04:30:56 -0400

    upload

--- projects/fear/main.js
+++ projects/fear/main.js
@@ -1,8 +1,20 @@
 $(function() {
   // Your interactions go here
   $( ".hide" ).click(function() {
-   $(".block1").toggle();
-   $(".block2").toggle();
+   $(".block1").hide();
+   $(".hide").hide();
+   $(".hide2").show();
+  });
+  $( ".hide2" ).click(function() {
+   $(".block1-2").hide();
+   $(".reset").show();
+   $(".hide2").hide();
+  });
+  $( ".reset" ).click(function() {
+   $(".block1").show();
+   $(".block1-2").show();
+   $(".hide").show();
+   $(".reset").hide();
   });
  
   

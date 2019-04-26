b1e62af9440daee44a1f4274f40bb8b93767017e
pana005 <pana005@newschool.edu>
2019-04-26 01:13:26 -0400

    poster

---
+++ in-class activities/mad lib/main.js
@@ -0,0 +1,30 @@
+$(function() {
+	var urls = [
+  		"https://kquea402.github.io/api.json",
+  		"https://glics998.github.io/api.json",
+  		"https://colindharrington.github.io/api.json",
+  		"https://hoant626.github.io/api.json",
+  		"https://karla772.github.io/api.json",
+  		"https://leeh779.github.io/api.json",
+  		"https://lis874.github.io/api.json",
+  		"https://phoebechloee.github.io/api.json",
+  		"https://luw779.github.io/api.json",
+  		"https://mullg389.github.io/api.json",
+  		"https://pana005.github.io/api.json",
+  		"https://raos130.github.io/api.json",
+  		"https://samsj948.github.io/api.json",
+  		"https://shaha129.github.io/api.json",
+  		"https://Mwedd9.github.io/api.json",
+  		"https://zhenx804.github.io/api.json"
+	];
+
+	$('.button').click(function(){
+		console.log($ (this));
+		var url = $(this.data('url');
+		console.log(url);
+
+	}).fail(function(error){
+		
+	});
+
+});
---
+++ projects/api/main.js
@@ -0,0 +1,6 @@
+$(function(){
+	console.log('hello!');
+	var url="https://api.dribbble.com/v2/user/shots?access_token='dac9b7bdfff6e503ee47a7b1b9b94b5d2e974efe77703ebd76fef176e6714d77"
+	console.log(url);
+	
+});
